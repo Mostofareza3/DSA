8c42165db1e900733abb97afc1c29edc639e50e6
pro-merndev <pro.merndev3@gmail.com>
2025-03-17 11:59:21 +0600

    Recursion

---
+++ recursion/hello.js
@@ -0,0 +1,15 @@
+
+
+function A(){
+    return `Mostofa`
+}
+
+function B(){
+    return `my name is: ${A()}`
+}
+
+function C(){
+    return `Hello! ${B()}`
+}
+
+console.log(C())
---
+++ recursion/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+function A(){
+    return `Mostofa`
+}
+
+function B(){
+    return `my name is: ${A()}`
+}
+
+function C(){
+    return `Hello! ${B()}`
+}
+
+console.log(C())
